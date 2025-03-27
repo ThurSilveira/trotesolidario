283c362286cbb1ae6a4721dc4d855b62e8711408
ThurSilveira <arthursilveira13000@gmail.com>
2025-03-27 19:54:36 -0300

    calculadora

--- trote/js/js.js
+++ trote/js/js.js
@@ -5,10 +5,9 @@ function calcular(){
     let mascote, homenagem 
     // recupera o valor do mascote digitado pelo usuario
     mascote = document.getElementById ("mascote").value
-    alert(mascote)
     // recupera o valor da homenagem digitado pelo usuario
     homenagem = document.getElementById("homenagem").value
-    alert(homenagem)
     let soma = Number(mascote) + Number(homenagem) 
-    alert(soma)
+    // retorna o valor ao html
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
