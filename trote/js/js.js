7efbf70c9a23d7a7bb2be5da0809976e00fb24eb
ThurSilveira <arthursilveira13000@gmail.com>
2025-04-02 19:08:24 -0300

    aa

--- trote/js/js.js
+++ trote/js/js.js
@@ -2,34 +2,51 @@ function calcular(){
     // vamos criar duas variaveis
     // JS as variaveis nao possuem tipo
     // let variavel
-    let mascote, homenagem, kitAvulso, suplementoAvulso, sangueAdicional, arroz5Avulso, arroz1Avulso, feijao2Avulso, feijao1Avulso, macarrao, oleo
+    let mascote, homenagem, kit, suplemento, sangue, arroz5, arroz1, feijao2, feijao1, macarrao, oleo, soma, equipes
     // recupera o valor do mascote digitado pelo usuario
-    mascote = document.getElementById ("mascote").value
+    mascote = Number(document.getElementById ("mascote").value)
     // recupera o valor da homenagem digitado pelo usuario
-    homenagem = document.getElementById("homenagem").value
+    homenagem = Number(document.getElementById("homenagem").value)
     //recupera a qtnd de litros de leite
-    leite = document.getElementById("leite").value
+    leite = Number(document.getElementById("leite").value)
     //recuperar a soma avulsa de kits alimentação
-    kitAvulso = document.getElementById("kitAvulso").value
+    kit = Number(document.getElementById("kit").value)
     //recuperar a soma suplemento avulso
-    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    suplemento = Number(document.getElementById("suplemento").value)
     //recuperar a soma sangue adicional
-    sangueAdicional = document.getElementById("sangueAdicional").value
+    sangue = Number(document.getElementById("sangue").value)
     // recuperar a soma arroz 5 avulso
-    arroz5Avulso = document.getElementById("arroz5Avulso").value
+    arroz5 = Number(document.getElementById("arroz5").value)
     //recupera valor arroz 1 avulso
-    arroz1Avulso = document.getElementById("arroz1Avulso").value
+    arroz1 = Number(document.getElementById("arroz1").value)
     //recupera valor feijao 2 avulso
-    feijao2Avulso = document.getElementById("feijao2Avulso").value
+    feijao2 = Number(document.getElementById("feijao2").value)
     //recupera valor feijao 1
-    feijao1Avulso = document.getElementById("feijao1Avulso").value
+    feijao1 = Number(document.getElementById("feijao1").value)
     // recupera maccarao
-    macarrao = document.getElementById("macarrao").value
+    macarrao = Number(document.getElementById("macarrao").value)
     // recupera oleo
-    oleo = document.getElementById("oleo").value
+    oleo = Number(document.getElementById("oleo").value)
     //calcular a soma
-    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite)) + (30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso)) + (20 * Number(sangueAdicional)) + (5 * Number(arroz5Avulso)) + (Number(arroz1Avulso)) + (2 * Number(feijao2Avulso)) + (Number(feijao1Avulso)) + (0.5 * Number(macarrao)) + (Number (oleo))
+    soma = mascote + homenagem + (2 * leite)  + (15 * suplemento) + (20 * sangue) + (5 * arroz5) + arroz1 + (2 * feijao2) + feijao1 + (0.5 * macarrao) + oleo
     // retorna o valor ao html
     //template string
+    equipes = document.getElementById("equipes").value
+    if (equipes == "Laranja"){
+        if (kit >= 106){
+            soma = soma + 5000 + ((kit-106) * 30)
+        }
+        else if (kit >= 85){
+            soma = soma + 4000 + ((kit-85) * 30)
+        }
+        else if (kit >= 53){
+            soma = soma + 2500 + ((kit-53) * 30)
+        }
+        else if (kit >= 21){
+            soma = soma + 1000 + ((kit-21) * 30)
+        }
+    }
+
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
+
